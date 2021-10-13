dea2bd14e5924e3ccdc8b8216767ca79ae9975ea
Maurice Smith <Xmaurice.smith@gmail.com>
2021-10-13 16:52:30 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks) {
+   return Math.abs(42 - blocks) 
+}
+
+function distanceFromHqInFeet(blocks) {
+   return distanceFromHqInBlocks(blocks) * 264 
+    
+
+}
+
+function distanceTravelledInFeet(a, b) {
+    return Math.abs(a-b) * 264
+}
+
+function calculatesFarePrice(a, b){
+    let distance = Math.abs(a-b) * 264 
+        if (distance < 400) {
+            return 0
+        }else if (distance < 2000) {
+            return (distance - 400) * .02
+        }else if ( distance < 2500) {
+            return 25
+        }
+        else {
+            return 'cannot travel that far'
+        }
+        
+
+}
