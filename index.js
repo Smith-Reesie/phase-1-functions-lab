5a4406b78fa1aee00f29e5a2fea3d2cd6a4f2f9d
Maurice Smith <Xmaurice.smith@gmail.com>
2021-10-23 19:33:33 -0400

    commit tree 03b1c23dc4fc19969d5f5edf71e5541ab673f56f

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 // Code your solution in this file!
 
-function distanceFromHqInBlocks(blocks) {
-   return Math.abs(42 - blocks) 
+
+function distanceFromHqInBlocks(block){
+    const hQ = 42
+    return Math.abs(`${block}` - hQ)
 }
 
-function distanceFromHqInFeet(blocks) {
-   return distanceFromHqInBlocks(blocks) * 264 
-    
+distanceFromHqInBlocks(43);
+distanceFromHqInBlocks(50);
+distanceFromHqInBlocks(34);
 
+function distanceFromHqInFeet(block) {
+     return  distanceFromHqInBlocks(block) * 264 
 }
 
-function distanceTravelledInFeet(a, b) {
-    return Math.abs(a-b) * 264
+function distanceTravelledInFeet(a,b) {
+    return Math.abs((a-b) * 264)
 }
 
-function calculatesFarePrice(a, b){
-    let distance = Math.abs(a-b) * 264 
-        if (distance < 400) {
-            return 0
-        }else if (distance < 2000) {
-            return (distance - 400) * .02
-        }else if ( distance < 2500) {
-            return 25
-        }
-        else {
-            return 'cannot travel that far'
-        }
-        
+distanceTravelledInFeet(43, 48);
+distanceTravelledInFeet(50, 60);
+distanceTravelledInFeet(34, 28);
 
+function calculatesFarePrice(a, b) {
+    let distance = Math.abs(a-b) * 264
+        if(distance <= 400) {
+                return 0 
+            }else if(distance < 2000) {
+                return (distance - 400) * .02
+            }else if (distance < 2500)
+                return 25
+            else 
+                return 'cannot travel that far'
 }
